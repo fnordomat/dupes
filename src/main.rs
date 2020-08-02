bcca61f40de50b0b00c98d43ef1a9ad12faff986
fnordomat <46D46D1246803312401472B5A7427E237B7908CA>
2020-08-02 08:09:40 +0200

    update dependency versions; optimization: hash excluded files (from -D) only when and if their size comes up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![feature(bufreader_seek_relative)]
 #![allow(non_snake_case)]
 
+extern crate anyhow;
 extern crate clap;
 extern crate libc;
 extern crate regex;
@@ -12,17 +13,15 @@ use clap::{App, Arg};
 use regex::Regex;
 use sha2::{Digest, Sha256};
 use std::collections::{BTreeMap, BTreeSet};
-use std::io;
 use std::io::{BufRead, BufReader};
 use std::path::Path;
 use walkdir::{Result, WalkDir};
 
 fn collect<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
     directories: Vec<P>,
-    quiet: bool,
     exclude_path_regex: &Option<Regex>,
-) -> BTreeMap<u64, BTreeSet<Vec<u8>>> {
-    let mut map: BTreeMap<u64, BTreeSet<_>> = BTreeMap::new();
+) -> BTreeMap<u64, BTreeSet<std::path::PathBuf>> {
+    let mut size_map: BTreeMap<u64, BTreeSet<std::path::PathBuf>> = BTreeMap::new();
 
     let isMatchOK = |s: &str| match &exclude_path_regex {
         Some(ex) => !ex.is_match(&s),
@@ -45,7 +44,8 @@ fn collect<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
             match entry.metadata() {
                 Ok(m) => {
                     // no size filter here, just want to exclude files like these
-                    map.entry(m.len())
+                    size_map
+                        .entry(m.len())
                         .or_insert_with(BTreeSet::new)
                         .insert(entry.into_path());
                 }
@@ -54,43 +54,29 @@ fn collect<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
         }
     }
 
-    let mut output_map = BTreeMap::new();
+    size_map
+}
 
-    for (size, set) in map.iter() {
-        let mut hashes = BTreeSet::new();
-        for entry in set {
-            let maybe_f = std::fs::File::open(entry);
-            if let Ok(f) = maybe_f {
-                let mut reader = BufReader::with_capacity(8192, f);
-                let mut hasher = Sha256::default();
-                'reading: loop {
-                    let consumed = match reader.fill_buf() {
-                        Ok(bytes) => {
-                            hasher.input(bytes);
-                            bytes.len()
-                        }
-                        Err(ref error) => {
-                            if !quiet { println!(
-                                "{:?} error reading file {:}",
-                                error.kind(),
-                                entry.display()
-                            ); }
-                            break 'reading;
-                        }
-                    };
-                    reader.consume(consumed);
-                    if consumed == 0 {
-                        break 'reading;
-                    }
-                }
-                let hashvec = hasher.result().as_slice().to_vec();
-                hashes.insert(hashvec);
+fn hashvec_from_reader<'a>(reader: &'a mut dyn BufRead) -> std::result::Result<Vec<u8>, std::io::Error> {
+    let mut hasher = Sha256::new();
+
+    'reading: loop {
+        let consumed = match reader.fill_buf() {
+            Ok(bytes) => {
+                hasher.update(bytes);
+                bytes.len()
             }
+            Err(error) => {
+                return Err(error);
+            }
+        };
+        reader.consume(consumed);
+        if consumed == 0 {
+            break 'reading;
         }
-        output_map.insert(*size, hashes);
     }
 
-    output_map
+    Ok(hasher.finalize().to_vec())
 }
 
 fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
@@ -101,18 +87,20 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
     show_non_duplicates: bool,
     always_hash: bool,
     exclude_path_regex: &Option<Regex>,
-    exclude_size_hash: BTreeMap<u64, BTreeSet<Vec<u8>>>,
-) -> io::Result<()> {
-    let mut map: BTreeMap<u64, BTreeSet<_>> = BTreeMap::new();
+    exclude_sizemap: BTreeMap<u64, BTreeSet<std::path::PathBuf>>,
+) -> std::result::Result<(), anyhow::Error> {
+    let mut size_map: BTreeMap<u64, BTreeSet<_>> = BTreeMap::new();
 
     let isMatchOK = |s: &str| match &exclude_path_regex {
-        Some(ex) => {
-            !ex.is_match(&s)
-        },
+        Some(ex) => !ex.is_match(&s),
         None => true,
     };
 
-    let mut output_table : Vec<(std::string::String, u64, std::collections::BTreeSet<&std::path::PathBuf>)> = vec![];
+    let mut output_table: Vec<(
+        std::string::String,
+        u64,
+        std::collections::BTreeSet<&std::path::PathBuf>,
+    )> = vec![];
     let quiet = emit_json;
 
     for path in directories {
@@ -136,7 +124,8 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
                             continue 'walking;
                         }
                     }
-                    map.entry(m.len())
+                    size_map
+                        .entry(m.len())
                         .or_insert_with(BTreeSet::new)
                         .insert(entry.into_path());
                 }
@@ -145,12 +134,16 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
         }
     }
 
-    'iterating : for (size, set) in map.iter() {
+    'iterating: for (size, set) in size_map.iter() {
         if let Some(max_size) = &avoid_compare_if_larger_than {
             if size > max_size {
-                if !quiet { println!("{:} (avoiding disambiguation)", size); }
+                if !quiet {
+                    println!("{:} (avoiding disambiguation)", size);
+                }
                 for entry in set {
-                    if !quiet { println!("  {:}", entry.display()); }
+                    if !quiet {
+                        println!("  {:}", entry.display());
+                    }
                 }
                 continue;
             }
@@ -161,10 +154,14 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
         if set.len() == 1 {
             if show_non_duplicates && !always_hash {
                 if !size_header_shown {
-                    if !quiet { println!("{:}", size); }
+                    if !quiet {
+                        println!("{:}", size);
+                    }
                 }
                 for entry in set {
-                    if !quiet { println!("  {:}", entry.display()); }
+                    if !quiet {
+                        println!("  {:}", entry.display());
+                    }
                 }
 
                 if emit_json {
@@ -181,44 +178,52 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
         let mut hashbins = BTreeMap::new();
         for entry in set {
             let maybe_f = std::fs::File::open(entry);
-            let excluded_hash_set = exclude_size_hash.get(size);
+
+            let potentially_excluded_file_set = exclude_sizemap.get(size);
+
+            // hash excluded files (from -D) only when and if the size comes up
+            let excluded_hash_set : BTreeSet<Vec<u8>> =
+                potentially_excluded_file_set.map(|fs| fs.into_iter().map(|f| {
+                    let maybe_f = std::fs::File::open(f);
+                    match maybe_f {
+                        Ok(f) => {
+                            let mut excluded_file_reader = BufReader::with_capacity(8192, f);
+                            hashvec_from_reader(&mut excluded_file_reader).map_err(
+                                |error| {
+                                    if !quiet {
+                                        println!("{:?} error reading file {:}", error.kind(), entry.display());
+                                    }
+                                    error
+                                }
+                            )
+                        }
+                        Err(error) => Err(error),
+                    }
+                }))
+                    .map( |x| x.filter_map(std::result::Result::ok).collect() )
+                    .unwrap_or( BTreeSet::new() );
+
             if let Ok(f) = maybe_f {
                 let mut reader = BufReader::with_capacity(8192, f);
-                let mut hasher = Sha256::default();
-                'reading: loop {
-                    let consumed = match reader.fill_buf() {
-                        Ok(bytes) => {
-                            hasher.input(bytes);
-                            bytes.len()
-                        }
-                        Err(ref error) => {
-                            if !size_header_shown {
-                                if !quiet { println!("{:}", size); }
-                                size_header_shown = true;
+
+                let hashvec = hashvec_from_reader(&mut reader).map_err(
+                    |error| {
+                        if !size_header_shown {
+                            if !quiet {
+                                println!("{:}", size);
                             }
-                            if !quiet { println!(
-                                "{:?} error reading file {:}",
-                                error.kind(),
-                                entry.display()
-                            ); }
-                            break 'reading;
+                            size_header_shown = true;
                         }
-                    };
-                    reader.consume(consumed);
-                    if consumed == 0 {
-                        break 'reading;
+                        if !quiet {
+                            println!("{:?} error reading file {:}", error.kind(), entry.display());
+                        }
+                        error
                     }
-                }
-                let hashvec = hasher.result();
+                )?;
 
                 // Exclude files present in excluded-files map at this stage:
-                let present_in_excluded = match excluded_hash_set {
-                    None => {false}
-                    Some(set) => {
-                        set.contains(&hashvec.as_slice().to_vec())
-                    }
-                };
-                
+                let present_in_excluded = excluded_hash_set.contains(&hashvec.as_slice().to_vec());
+
                 if !present_in_excluded {
                     hashbins
                         .entry(hashvec)
@@ -234,19 +239,20 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
                 continue;
             }
             if !size_header_shown {
-                if !quiet { println!("{:}", size); }
+                if !quiet {
+                    println!("{:}", size);
+                }
                 size_header_shown = true;
             }
             if !quiet {
-                println!("  {:X}", key);
+                println!("  {}", toUpperHex(&key));
                 for entry in bin {
                     println!("    {:}", entry.display());
                 }
             }
 
             if emit_json {
-                output_table.push(
-                    (format!("{:X}", &key), *size, bin.clone()));
+                output_table.push((toUpperHex(&key), *size, bin.clone()));
             }
         }
     }
@@ -259,14 +265,17 @@ fn walk<P: AsRef<Path> + std::cmp::Eq + std::hash::Hash>(
     Ok(())
 }
 
-fn main() {
+fn toUpperHex(vec : &[u8]) -> String {
+    vec.iter().map(|x| format!("{:X}", x)).collect::<String>()
+}
 
+fn main() {
     unsafe {
         libc::signal(libc::SIGPIPE, libc::SIG_DFL);
     }
 
     let matches = App::new("Dupes")
-        .version("0.1.1")
+        .version("0.2.0")
         .author("fnordomat <GPG:46D46D1246803312401472B5A7427E237B7908CA>")
         .about("Finds duplicate files (according to SHA256)")
         .arg(Arg::with_name("dir")
@@ -284,7 +293,7 @@ fn main() {
         .arg(Arg::with_name("show_non_duplicates")
              .short("S")
              .long("show-non-duplicates")
-             .help("List also files that are unique"))
+             .help("List also files that are unique (automatically true if -D is used)"))
         .arg(Arg::with_name("always_hash")
              .short("A")
              .long("always-hash")
@@ -337,10 +346,9 @@ fn main() {
         .values_of("exclude_path")
         .map_or([].to_vec(), |x| x.collect());
 
-    let show_non_duplicates = matches.occurrences_of("show_non_duplicates") > 0
-        || !myantidirs.is_empty();
-    let always_hash = matches.occurrences_of("always_hash") > 0
-        || !myantidirs.is_empty();
+    let show_non_duplicates =
+        matches.occurrences_of("show_non_duplicates") > 0 || !myantidirs.is_empty();
+    let always_hash = matches.occurrences_of("always_hash") > 0 || !myantidirs.is_empty();
     let emit_json = matches.occurrences_of("emit_json") > 0;
 
     let exclude_path_regex = if exclude_exprs.is_empty() {
@@ -357,13 +365,10 @@ fn main() {
         avoid_compare_if_larger_than = None
     }
 
-    let quiet = emit_json; // may become an independent option in the future
+    // let quiet = emit_json; // may become an independent option in the future
 
-    let excluded_map : BTreeMap<u64, BTreeSet<Vec<u8>>> = collect(
-        myantidirs,
-        quiet,
-        &exclude_path_regex,
-    );
+    let excluded_sizemap: BTreeMap<u64, BTreeSet<std::path::PathBuf>> =
+        collect(myantidirs, &exclude_path_regex);
 
     let _ = walk(
         mydirs,
@@ -373,6 +378,6 @@ fn main() {
         show_non_duplicates,
         always_hash,
         &exclude_path_regex,
-        excluded_map,
+        excluded_sizemap,
     );
 }
